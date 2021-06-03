846870b236c0a7e236f276f6046736680343f3ee
Sean Siddens <seansiddens@gmail.com>
2021-06-03 14:26:19 -0700

    Fixed tridagonal

--- bohemian.py
+++ bohemian.py
@@ -2,6 +2,7 @@ import numpy as np
 import random 
 import math
 import cmath
+from numpy.linalg import LinAlgError
 from tqdm import tqdm
 from PIL import Image
 from matplotlib import cm
@@ -23,8 +24,68 @@ def complex_to_image(z, width, height, real_range, imag_range, centered_at):
 
     return (int(x), int(y))
 
-def tridiagonal(im_arr, width, height, real_range, imag_range, centered_at, cmap):
-    pass
+# Plot of eigenvalues of random 10x10 tridiagonal matrices
+def tridiagonal(im_arr, width, height, real_range, imag_range, centered_at, cmap, samples):
+    n = 10 # n x n matrix
+    mono_coloring = False
+
+    # Array for tracking # of eigvals for each pixel
+    counts = np.zeros((width, height), dtype=np.uint64)
+
+    # Non-zero elements of the matrix are sampled from this sequence
+    values = np.array([complex(0, 0), complex(1, 0), complex(-1, 0), complex(0, 1), complex(0, -1), 
+                    complex(20, 0), complex(-20, 0), complex(0, 20), complex(0, -20)])
+
+    # Complute eigenvalues of samples number of matrices 
+    print("Computing eigenvalues...")
+    for _ in tqdm(range(samples)):
+        # Entries not in the sub-, super-, or main-diagonals are zero
+        mat = np.full((n, n), complex(0, 0)) 
+        # Set entries in sub- and super-diagonal
+        for i in range(1, n): 
+            mat[i, i-1] = random.choice(values)
+            mat[i-1, i] = random.choice(values)
+        # Set entries along main diagonal
+        for i in range(n):
+            mat[i, i] = random.choice(values)
+        
+        # Get eigvenvalues of the matrix
+        try:
+            eigvals = np.linalg.eigvals(mat)
+        except LinAlgError:
+            print("Eigenvalue error")
+
+
+        # Convert each eigvenvalue to a point on image
+        for z in eigvals:
+            x, y = complex_to_image(z, width, height, real_range, imag_range, centered_at)
+            # Check if it's in image
+            if (0 <= x < width) and (0 <= y < height):
+                counts[y, x] += 1
+    print("Done!")
+
+    max_count = np.max(counts)
+    print("Max count:", max_count)
+
+    # Color image depending on density of eiginvalues for each pixel
+    print("Coloring final image...")
+    for y in tqdm(range(height)):
+        for x in range(width):
+            if counts[y, x] != 0:
+                if mono_coloring is True:
+                    im_arr[y, x] = 255
+                else: 
+                    brightness = math.log(counts[y, x]) / math.log(max_count)
+                    gamma = 2.2
+                    brightness = math.pow(brightness, 1/gamma)
+                    rgba = cmap(brightness)
+                    im_arr[y, x, 0] = int(255 * rgba[0])
+                    im_arr[y, x, 1] = int(255 * rgba[1])
+                    im_arr[y, x, 2] = int(255 * rgba[2])
+
+    im = Image.fromarray(im_arr)
+    return im
+
 
 # Returns image of "eigenfish", plot of eigenvalues of matrices w/ entries in
 # {-1, 0, 1}, and some entries taking on random continous values in a specified range
@@ -57,6 +118,7 @@ def eigenfish(im_arr, width, height, real_range, imag_range, centered_at):
 
     im = Image.fromarray(im_arr)
     return im
+
 
 # Plot of eigenvalues of companion matrices of littlewood polynomials. 
 # Coefficients take on values in {-1, 1}
@@ -140,6 +202,7 @@ def littlewood(im_arr, width, height, real_range, imag_range, centered_at, cmap,
     im = Image.fromarray(im_arr)
     return im
 
+
 # Computes all complex roots of all Littlewood polynomials of specified degree,
 # and writes them out to a file
 def compute_littlewood_roots(degree, outfile):
@@ -186,36 +249,37 @@ def compute_littlewood_roots(degree, outfile):
     fd.close()
 
 
-
 if __name__ == "__main__":
     # Size of complex window w/ respect to a center focal point
     centered_at = complex(0, 0)
     real_offset = (-1, 1)
     imag_offset = (-1, 1)
-
     real_range = real_offset[1] - real_offset[0]
     imag_range = imag_offset[1] - imag_offset[0]
-    scale = 2
+    scale = 48
     real_range *= scale
     imag_range *= scale
 
     # Set width and height of image depending on aspect ratio
     aspect_ratio = real_range / imag_range
-    width = 1024 * 11 
+    width = 1024 * 4
     height = int(width * 1 / aspect_ratio)
 
     # # Initialize image array
     im_arr = np.zeros((height, width, 3), dtype=np.uint8)
 
+    # Number of samples we want to take from set of matrices
+    samples = 10000000
+
     # # Initialize color map
-    # cmap = cm.get_cmap("hot")
+    # cmap = cm.get_cmap("jet")
     # cmap = cm.get_cmap("viridis")
-    cmap = cm.get_cmap("nipy_spectral")
-
-    degree = 19
-
+    # cmap = cm.get_cmap("nipy_spectral")
+    cmap = cm.get_cmap("hot")
+    
     # im = eigenfish(im_arr, width, height, real_range, imag_range, centered_at)
-    im = littlewood(im_arr, width, height, real_range, imag_range, centered_at, cmap, "littlewood_roots_deg23.txt", degree)
+    # im = littlewood(im_arr, width, height, real_range, imag_range, centered_at, cmap)
+    im = tridiagonal(im_arr, width, height, real_range, imag_range, centered_at, cmap, samples)
 
     print("Saving image...")
     im.save("out.png")
